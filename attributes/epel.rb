0a547c2238b41c13f7823cacfe8c41352c6992d2
Eric G. Wolfe <wolfe21@marshall.edu>
2013-12-13 15:55:59 -0500

    Fixing EL6 attributes
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- attributes/epel.rb
+++ attributes/epel.rb
@@ -7,8 +7,8 @@ when 5
   default['yum']['epel']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
 when 6
   default['yum']['epel']['description'] = 'Extra Packages for Enterprise Linux 6 - $basearch'
-  default['yum']['epel']['mirrorlist'] = 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch'
-  default['yum']['epel']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL'
+  default['yum']['epel']['mirrorlist'] = 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  default['yum']['epel']['gpgkey'] = 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
 end
 
 default['yum']['epel']['failovermethod'] = 'priority'
