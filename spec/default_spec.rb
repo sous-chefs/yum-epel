73efe2c13002760fc42a425929843f6d708fefb0
Sean OMeara <someara@opscode.com>
2014-02-12 17:27:12 -0500

    updating test harness

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'yum-epel::default' do
   context 'yum-epel::default uses default attributes' do
-    let(:chef_run) { ChefSpec::Runner.new(:step_into => ['yum_repository']).converge(described_recipe) }
+    let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
     %w{
       epel
@@ -14,10 +14,6 @@ describe 'yum-epel::default' do
       }.each do |repo|
       it "creates yum_repository[#{repo}]" do
         expect(chef_run).to create_yum_repository(repo)
-      end
-
-      it "steps into yum_repository and creates template[/etc/yum.repos.d/#{repo}.repo]" do
-        expect(chef_run).to render_file("/etc/yum.repos.d/#{repo}.repo")
       end
     end
 
