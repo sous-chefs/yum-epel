72591947d755676dcd60b83b013d332c72d7c9a7
Tim Smith <tsmith@chef.io>
2015-09-07 13:25:10 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'yum-epel'
-maintainer 'Chef'
-maintainer_email 'Sean OMeara <someara@chef.io>'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-epel'
 version '0.6.2'
