7f1880a6e39ec61b97363f05d081a5d4bc6ca2b1
Tim Smith <tsmith84@gmail.com>
2015-09-10 21:58:55 -0700

    Update copyright

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,9 @@
 #
 # Author:: Sean OMeara (<someara@chef.io>)
-# Recipe:: yum-epel::default
+# Cookbook Name:: yum-epel
+# Recipe:: default
 #
-# Copyright 2013, Chef
+# Copyright 2013-2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
