a444a926555cd0a60e0b061cc87290a717ad020b
Tim Smith <tsmith84@gmail.com>
2015-09-22 23:19:36 -0700

    Improve description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'yum-epel'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-epel'
+description 'Installs and configures the EPEL Yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.6.2'
 
