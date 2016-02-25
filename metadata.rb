909892d5672bf0a3535181c679b28bcadc3c82f6
212050466 <josh.schneider@ge.com>
2016-02-25 13:51:17 -0600

    version increment and changelog update

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures the EPEL Yum repository'
-version '0.6.5'
+version '0.6.6'
 
 depends 'yum', '~> 3.10.0'
 
