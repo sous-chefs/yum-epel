717b5a7b141a970435c8c9a79790cc758705c5cd
Tim Smith <tsmith84@gmail.com>
2016-04-27 10:47:16 -0700

    Add long_description metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures the EPEL Yum repository'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.6.6'
 
 depends 'yum', '>= 3.6.3'
