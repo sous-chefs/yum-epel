25eb117659cbc9ceb631111f49f2a80ea253eb71
Tim Smith <tsmith@chef.io>
2017-03-21 15:58:35 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'yum-epel'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs and configures the EPEL Yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.1'
