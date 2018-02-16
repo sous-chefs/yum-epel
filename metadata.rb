0aebbc2da7ace6b17f433a277c340d2bd356b6e2
Tim Smith <tsmith@chef.io>
2018-02-16 11:53:45 -0800

    Require Chef 12.14+ and remove the compat_resource dep
    compat_resource causes a lot of issues and we should just dep on a more
    modern chef-client release here.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,10 @@ description 'Installs and configures the EPEL Yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.2'
 
-depends 'compat_resource', '>= 12.16.3'
-
 %w(amazon centos oracle redhat scientific zlinux).each do |os|
   supports os
 end
 
 source_url 'https://github.com/chef-cookbooks/yum-epel'
 issues_url 'https://github.com/chef-cookbooks/yum-epel/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.14' if respond_to?(:chef_version)
