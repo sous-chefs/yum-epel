ed1a948e71c3edf9c36ed8227fa35ee3d0f48c67
Dan Webb <dan.webb@damacus.io>
2026-05-05 12:49:33 +0100

    fix: avoid epel all suite repo metadata lookups

--- test/integration/all/controls/all_spec.rb
+++ test/integration/all/controls/all_spec.rb
@@ -1,83 +1,48 @@
 # frozen_string_literal: true
 
 os_release = os.name == 'amazon' ? 9 : os.release.to_i
-basearch = command('rpm --eval %{_arch}').stdout.strip
+basearch = '$basearch'
 stream = file('/etc/os-release').content.match?('Stream')
 epel_next_expected = stream && os_release < 10
 testing_repo = os_release >= 10 ? "epel-z-testing-#{os_release}" : "testing-epel#{os_release}"
 testing_debug_repo = os_release >= 10 ? "epel-z-testing-debug-#{os_release}" : "testing-debug-epel#{os_release}"
 testing_source_repo = os_release >= 10 ? "epel-z-testing-source-#{os_release}" : "testing-source-epel#{os_release}"
 
-describe yum.repo 'epel' do
-  it { should exist }
-  it { should be_enabled }
-  its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{os_release}&arch=#{basearch}" }
+def repo_file(repo)
+  file("/etc/yum.repos.d/#{repo}.repo")
 end
 
-describe yum.repo 'epel-debuginfo' do
-  it { should exist }
-  it { should be_enabled }
-  its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-debug-#{os_release}&arch=#{basearch}" }
+def describe_repo(repo, mirrorlist)
+  describe repo_file(repo) do
+    it { should exist }
+    its('content') { should include "[#{repo}]" }
+    its('content') { should include 'enabled=1' }
+    its('content') { should include "mirrorlist=#{mirrorlist}" }
+  end
 end
 
-describe yum.repo 'epel-source' do
-  it { should exist }
-  it { should be_enabled }
-  its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-source-#{os_release}&arch=#{basearch}" }
-end
+describe_repo('epel', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-#{os_release}&arch=#{basearch}")
 
-describe yum.repo 'epel-testing' do
-  it { should exist }
-  it { should be_enabled }
-  its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=#{testing_repo}&arch=#{basearch}" }
-end
+describe_repo('epel-debuginfo', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-debug-#{os_release}&arch=#{basearch}")
 
-describe yum.repo 'epel-testing-debuginfo' do
-  it { should exist }
-  it { should be_enabled }
-  its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=#{testing_debug_repo}&arch=#{basearch}" }
-end
+describe_repo('epel-source', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-source-#{os_release}&arch=#{basearch}")
 
-describe yum.repo 'epel-testing-source' do
-  it { should exist }
-  it { should be_enabled }
-  its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=#{testing_source_repo}&arch=#{basearch}" }
-end
+describe_repo('epel-testing', "https://mirrors.fedoraproject.org/mirrorlist?repo=#{testing_repo}&arch=#{basearch}")
+
+describe_repo('epel-testing-debuginfo', "https://mirrors.fedoraproject.org/mirrorlist?repo=#{testing_debug_repo}&arch=#{basearch}")
+
+describe_repo('epel-testing-source', "https://mirrors.fedoraproject.org/mirrorlist?repo=#{testing_source_repo}&arch=#{basearch}")
 
 if epel_next_expected
-  describe yum.repo 'epel-next' do
-    it { should exist }
-    it { should be_enabled }
-    its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-next-#{os_release}&arch=#{basearch}" }
-  end
+  describe_repo('epel-next', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-next-#{os_release}&arch=#{basearch}")
 
-  describe yum.repo 'epel-next-debuginfo' do
-    it { should exist }
-    it { should be_enabled }
-    its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-next-debug-#{os_release}&arch=#{basearch}" }
-  end
+  describe_repo('epel-next-debuginfo', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-next-debug-#{os_release}&arch=#{basearch}")
 
-  describe yum.repo 'epel-next-source' do
-    it { should exist }
-    it { should be_enabled }
-    its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-next-source-#{os_release}&arch=#{basearch}" }
-  end
+  describe_repo('epel-next-source', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-next-source-#{os_release}&arch=#{basearch}")
 
-  describe yum.repo 'epel-next-testing' do
-    it { should exist }
-    it { should be_enabled }
-    its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-testing-next-#{os_release}&arch=#{basearch}" }
-  end
+  describe_repo('epel-next-testing', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-testing-next-#{os_release}&arch=#{basearch}")
 
-  describe yum.repo 'epel-next-testing-debuginfo' do
-    it { should exist }
-    it { should be_enabled }
-    its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-testing-next-debug-#{os_release}&arch=#{basearch}" }
-  end
+  describe_repo('epel-next-testing-debuginfo', "https://mirrors.fedoraproject.org/mirrorlist?repo=epel-testing-next-debug-#{os_release}&arch=#{basearch}")
 
-  describe yum.repo 'epel-next-testing-source' do
-    it { should exist }
-    it { should be_enabled }
-    its('mirrors') { should cmp "https://mirrors.fedoraproject.org/mirrorlist?repo=testing-source-epel#{os_release}&arch=#{basearch}" }
-  end
+  describe_repo('epel-next-testing-source', "https://mirrors.fedoraproject.org/mirrorlist?repo=testing-source-epel#{os_release}&arch=#{basearch}")
 end
